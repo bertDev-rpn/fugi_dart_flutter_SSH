efed9ac2367146373771c2c8e3031519f7c4848f
robert-napitupulu <bertran.gbu@gmail.com>
2022-10-31 10:36:13 +0700

    Belajar SSH Github

---
+++ bin/Hallo_Wolrd.dart
@@ -0,0 +1,5 @@
+import 'package:fugi_dart_flutter/fugi_dart_flutter.dart' as fugi_dart_flutter;
+
+void main(List<String> arguments) {
+  print('Hello world');
+}
---
+++ test/fugi_dart_flutter_test.dart
@@ -0,0 +1,8 @@
+import 'package:fugi_dart_flutter/fugi_dart_flutter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
